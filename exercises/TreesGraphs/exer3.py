52064968dea4ee9788a2286389f8c1ce76099301
sasha novikov <novikov.ae@gmail.com>
2022-11-16 13:25:31 +0300

    exercise[4.3]: solution and tests

---
+++ exercises/TreesGraphs/exer3.py
@@ -0,0 +1,32 @@
+# List of Depths: Given a binary tree, design an algorithm which creates
+# a linked list of all the nodes at each depth (e.g., if you have a tree
+# with depth D, you'll have D linked lists).
+
+import unittest
+from exercises.LinkedList.implementation import SingleLinkedList
+from exer2 import create_tree
+
+
+def func(node, _list: SingleLinkedList):
+    if not node:
+        return
+    func(node.left, _list)
+    _list.addAtTail(node.value)
+    func(node.right, _list)
+
+
+class TestFunc(unittest.TestCase):
+    def setUp(self) -> None:
+        self.list = SingleLinkedList()
+
+    def test_one(self):
+        arr = [1, 2, 3, 4, 5]
+        tree = create_tree(arr, view=[])
+        func(tree, self.list)
+        self.assertEqual(self.list.view(), [1, 2, 3, 4, 5])
+
+    def test_two(self):
+        arr = [1, 3, 4, 7, 8, 10, 12, 14]
+        tree = create_tree(arr, view=[])
+        func(tree, self.list)
+        self.assertEqual(self.list.view(), [1, 3, 4, 7, 8, 10, 12, 14])
