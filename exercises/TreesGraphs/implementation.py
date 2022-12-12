4dc4e6f26cabdc1abfded5a4a4998d7c0748f6f4
sasha novikov <novikov.ae@gmail.com>
2022-12-12 14:49:13 +0300

    exercise[4.x]: binary tree

--- exercises/TreesGraphs/implementation.py
+++ exercises/TreesGraphs/implementation.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 class Node:
     def __init__(self, val):
         self.value = val
@@ -10,9 +13,38 @@ class BinaryTree:
         self.head = None
 
     def view(self):
-        return self.recr(self.head)
+        return self.view_recr(self.head)
 
-    def recr(self, node):
+    def view_recr(self, node):
         if not node:
             return []
-        return self.recr(node.left) + [node.value] + self.recr(node.right)
+        return [node.value] + self.view_recr(node.left) + self.view_recr(node.right)
+
+    def create_preorder(self, preorder: list):
+        sequence = iter(preorder)
+        self.head = self.create_recr(sequence)
+        return self.head
+
+    def create_recr(self, seq):
+        val = next(seq)
+        if val == '#':
+            return
+        node = Node(val)
+        node.left = self.create_recr(seq)
+        node.right = self.create_recr(seq)
+        return node
+
+
+class TestFunc(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tree = BinaryTree()
+
+    def test_one(self):
+        arr = [3, 2, 1, '#', '#', '#', 5, 4, '#', '#', 6, '#', '#']
+        self.tree.create_preorder(arr)
+        self.assertEqual([3, 2, 1, 5, 4, 6], self.tree.view())
+
+    def test_two(self):
+        arr = [3, '#', '#']
+        self.tree.create_preorder(arr)
+        self.assertEqual([3], self.tree.view())
