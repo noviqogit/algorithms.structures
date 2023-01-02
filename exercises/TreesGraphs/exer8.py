87f4d475e51c10408f8b8679859fe6646ccd0ec9
sasha novikov <novikov.ae@gmail.com>
2023-01-02 16:34:57 +0300

    exercise[4.8]: optimize solution

--- exercises/TreesGraphs/exer8.py
+++ exercises/TreesGraphs/exer8.py
@@ -3,55 +3,35 @@
 # NOTE: This is not necessarily a binary search tree.
 
 import unittest
-from exer2 import BalancedBinarySearchTree, Node
+from implementation import BinaryTree
 
 
 def common_ancestor(tree, one, two):
-    first = find_path(tree, one, ())
-    second = find_path(tree, two, ())
-    lenght = min(len(first), len(second)) - 1
-    first, second = first[:lenght], second[:lenght]
-    for node, _node in zip(first[::-1], second[::-1]):
-        if node == _node:
-            return node
-
-
-def find_path(tree, target, path):
     if not tree:
-        return
-    path += (tree.value,)
-    if tree == target:
-        return path
-    left = find_path(tree.left, target, path)
-    right = find_path(tree.right, target, path)
+        return None
+    if tree.value in (one, two):
+        return tree
+    left = common_ancestor(tree.left, one, two)
+    right = common_ancestor(tree.right, one, two)
+    if not left and not right:
+        return None
+    if left and right:
+        return tree
     return left if left else right
 
 
 class TestFunc(unittest.TestCase):
     def setUp(self) -> None:
-        self.tree = BalancedBinarySearchTree()
+        self.tree = BinaryTree()
 
     def test_one(self):
-        arr = [1, 2, 3, 4, 5]
-        self.tree.head = self.tree.create(arr)
-        node = self.tree.head
-        while node.right:
-            node = node.right
-        one = node.right = Node(node.value)
-        while node.left:
-            node = node.left
-        two = node.left = Node(self.tree.head.value)
-        self.assertEqual(5, common_ancestor(self.tree.head, one, two))
+        arr = [7, 2, 1, '#', '#', '#', 5, 4, '#', '#', 6, 1, '#', '#', '#']
+        head = self.tree.create_preorder(arr)
+        node = common_ancestor(head, 4, 6)
+        self.assertEqual(5, node.value)
 
     def test_two(self):
-        arr = [1, 2, 3, 4, 5]
-        self.tree.head = self.tree.create(arr)
-        node = self.tree.head
-        while node.left:
-            node = node.left
-        one = node.right = Node(self.tree.head.value)
-        node = self.tree.head
-        while node.right:
-            node = node.right
-        two = node
-        self.assertEqual(3, common_ancestor(self.tree.head, one, two))
+        arr = [7, 2, 1, '#', '#', '#', 5, 4, '#', '#', 6, 1, '#', '#', '#']
+        head = self.tree.create_preorder(arr)
+        node = common_ancestor(head, 1, 6)
+        self.assertEqual(7, node.value)
