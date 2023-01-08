a2c85bac9a39c6252b7ada73257484df2ca90c5d
sasha novikov <novikov.ae@gmail.com>
2023-01-08 16:03:59 +0300

    exercise[4.11]: refactor

--- exercises/TreesGraphs/exer11.py
+++ exercises/TreesGraphs/exer11.py
@@ -10,6 +10,11 @@ from random import choice
 
 class Solution(BalancedBinarySearchTree):
 
+    def set_tree(self, arr):
+        self.arr = arr
+        self.head = super().create_from_arr(arr)
+        return self.head
+
     def find(self, root, val):
         if not root:
             return None
@@ -19,16 +24,24 @@ class Solution(BalancedBinarySearchTree):
             return self.find(root.right, val)
         return self.find(root.left, val)
 
-    def insert(self, root, val):
+    def insert_recr(self, root, val):
         if not root:
             return Node(val)
         if root.value < val:
-            root.right = self.insert(root.right, val)
+            root.right = self.insert_recr(root.right, val)
         else:
-            root.left = self.insert(root.left, val)
+            root.left = self.insert_recr(root.left, val)
         return root
 
+    def insert(self, root, val):
+        self.arr.append(val)
+        return self.insert_recr(root, val)
+
     def delete(self, root, key):
+        self.arr.remove(key)
+        self.delete_recr(root, key)
+
+    def delete_recr(self, root, key):
         if not root:
             return None
         if root.value == key:
@@ -40,22 +53,22 @@ class Solution(BalancedBinarySearchTree):
             while node.left:
                 node = node.left
             root.val = node.val
-            root.right = self.delete(root.right, node.val)
+            root.right = self.delete_recr(root.right, node.val)
         if key > root.value:
-            root.right = self.delete(root.right, key)
+            root.right = self.delete_recr(root.right, key)
         if key < root.value:
-            root.left = self.delete(root.left, key)
+            root.left = self.delete_recr(root.left, key)
         return root
 
     def getRandomNone(self):
-        return choice(self.view())
+        return choice(self.arr())
 
 
 class TestFunc(unittest.TestCase):
     def setUp(self) -> None:
         self.tree = Solution()
         arr = [1, 2, 5, 7, 12]
-        self.root = self.tree.create_from_arr(arr)
+        self.root = self.tree.set_tree(arr)
 
     def test_one(self):
         self.assertTrue(self.tree.find(self.root, 1))
@@ -65,8 +78,8 @@ class TestFunc(unittest.TestCase):
 
     def test_three(self):
         self.tree.insert(self.root, 6)
-        self.assertEqual([1, 2, 5, 6, 7, 12], self.tree.view())
+        self.assertEqual([1, 2, 5, 7, 12, 6], self.tree.arr)
 
     def test_four(self):
         self.tree.delete(self.root, 2)
-        self.assertEqual([1, 5, 7, 12], self.tree.view())
+        self.assertEqual([1, 5, 7, 12], self.tree.arr)
