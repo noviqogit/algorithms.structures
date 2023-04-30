110bbfeae60047b0ef2ded34de77358c68bdca14
sasha novikov <novikov.ae@gmail.com>
2023-04-30 20:00:03 +0300

    Yandex test[exercise 1]: clean, simplified solution

--- Yandex test/exercise 1.py
+++ Yandex test/exercise 1.py
@@ -1,7 +1,8 @@
 n, m, q = map(int, input().split())
 
-# first value in every center uses to count resets
-centres = {i: [1 for _ in range(m + 1)] for i in range(1, n + 1)}
+servers = {i: [1 for _ in range(m)] for i in range(1, n + 1)}
+
+resets = {i: 1 for i in range(1, n + 1)}
 
 active_servers = {i: m for i in range(1, n + 1)}
 
@@ -10,35 +11,37 @@ metrics = {i: m for i in range(1, n + 1)}
 
 def reset(center):
     i = int(center)
-    resets = centres[i][0]
-    centres[i] = [1 for _ in range(m + 1)]
-    centres[i][0] += resets
+    resets[i] += 1
     active_servers[i] = m
-    metrics[i] = centres[i][0] * m
+    servers[i] = [1 for _ in range(m)]
+    metrics[i] = resets[i] * active_servers[i]
 
 
 def disable(center, server):
-    i, j = int(center), int(server)
-    if centres[i][j]:
-        centres[i][j] = 0
+    i, j = int(center), int(server) - 1
+    if servers[i][j]:
+        servers[i][j] = 0
         active_servers[i] -= 1
-        metrics[i] = centres[i][0] * active_servers[i]
+        metrics[i] = resets[i] * active_servers[i]
 
 
 def getmax():
-    pairs = sorted(metrics.items(), key=lambda x: (x[1], x[0]))
-    i, j = pairs[-1]
-    to_check = pairs[::-1]
-    for _i, _j in to_check:
-        if _j < j:
-            break
-        i = _i
-    print(i)
+    server, value = 1, 0
+    for i, metric in metrics.items():
+        if metric > value:
+            server, value = i, metric
+    print(server)
 
 
 def getmin():
-    pairs = sorted(metrics.items(), key=lambda x: (x[1], x[0]))
-    print(pairs[0][0])
+    server, value = 1, float('inf')
+    for i, metric in metrics.items():
+        if metric == 0:
+            server = i
+            break
+        if metric < value:
+            server, value = i, metric
+    print(server)
 
 
 events = {
